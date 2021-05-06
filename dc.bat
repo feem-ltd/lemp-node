1b41bb5d145840ece8363824b89e4f516573a0f5
Shigeo Nakamura <cityhunterplus@gmail.com>
2021-05-07 03:18:34 +0900

    docker-compose コマンドのヘルパーバッチファイル

--- dc.bat
+++ dc.bat
@@ -5,7 +5,7 @@ if "%1"=="" (
 
 REM 引数が指定されていな場合、結局空で何も指定されないので、
 REM stop と一つだけと、up -d のように二つだけでも大丈夫みたい。
-docker-compose -f docker-compose.production.yml %1 %2 %3
+docker-compose %1 %2 %3
 REM if not "%2"=="" (
 REM     docker-compose -f docker-compose.production.yml %1 %2
 REM ) else (
