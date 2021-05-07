87826e927e3e453eb0f8858a3968c5d9cbd3ddcb
Shigeo Nakamura <cityhunterplus@gmail.com>
2021-05-08 03:05:04 +0900

    実際の本番環境と producion をローカルで動作させるものを分離

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
 
-docker build -t feem/cashbook
+docker build --file="build/nginx/Dockerfile" -t feem/lemp-node-nginx .
+docker build --file="build/nuxt/Dockerfile.production" -t feem/lemp-node-nuxt .
+docker build --file="build/laravel/Dockerfile.production" -t feem/lemp-node-laravel .
